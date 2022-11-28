29f66e55e32af8d890a9307f602c5cb5b953158d
ETHAN23\pangy <Ethant Pang>
2022-11-28 18:29:07 +0800

    feat : add in auth controller features for driverController to show register and login page

--- app/Admin/Controllers/AuthController.php
+++ app/Admin/Controllers/AuthController.php
@@ -6,5 +6,101 @@ use Encore\Admin\Controllers\AuthController as BaseAuthController;
 
 class AuthController extends BaseAuthController
 {
+    /**
+     * @var string
+     */
+    protected $driverLoginView = 'driverLogin';
+    protected $driverRegisterView = 'driverRegister';
+
+    /**
+     * Show the login page.
+     *
+     * @return \Illuminate\Contracts\View\Factory|Redirect|\Illuminate\View\View
+     */
+    public function getDriverLogin()
+    {
+        if ($this->guard()->check()) {
+            return redirect($this->redirectPath());
+        }
+
+        return view($this->driverLoginView);
+    }
+
+    /**
+     * Show the login page.
+     *
+     * @return \Illuminate\Contracts\View\Factory|Redirect|\Illuminate\View\View
+     */
+    public function getDriverRegister()
+    {
+        if ($this->guard()->check()) {
+            return redirect($this->redirectPath());
+        }
+
+        return view($this->driverRegisterView);
+    }
+
+    /**
+     * Handle a login request.
+     *
+     * @param Request $request
+     *
+     * @return mixed
+     */
+    public function postDriverLogin(Request $request)
+    {
+        $this->loginValidator($request->all())->validate();
+
+        $credentials = $request->only([$this->username(), 'password']);
+        $remember = $request->get('remember', false);
+
+        if ($this->guard()->attempt($credentials, $remember)) {
+            return $this->sendLoginResponse($request);
+        }
+
+        return back()->withInput()->withErrors([
+            $this->username() => $this->getFailedLoginMessage(),
+        ]);
+    }
+
+    /**
+     * Model-form for driver setting.
+     *
+     * @return Form
+     */
+    protected function settingDrvierForm()
+    {
+        $class = config('admin.database.users_model');
+
+        $form = new Form(new $class());
+
+        $form->display('username', trans('admin.username'));
+        $form->text('name', trans('admin.name'))->rules('required');
+        $form->image('avatar', trans('admin.avatar'));
+        $form->password('password', trans('admin.password'))->rules('confirmed|required');
+        $form->password('password_confirmation', trans('admin.password_confirmation'))->rules('required')
+            ->default(function ($form) {
+                return $form->model()->password;
+            });
+
+        $form->setAction(admin_url('auth/setting'));
+
+        $form->ignore(['password_confirmation']);
+
+        $form->saving(function (Form $form) {
+            if ($form->password && $form->model()->password != $form->password) {
+                $form->password = Hash::make($form->password);
+            }
+        });
+
+        $form->saved(function () {
+            admin_toastr(trans('admin.update_succeeded'));
+
+            return redirect(admin_url('auth/setting'));
+        });
+
+        return $form;
+    }
+
 
 }
---
+++ config/driver.php
@@ -0,0 +1,49 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Laravel-admin name
+    |--------------------------------------------------------------------------
+    |
+    | This value is the name of laravel-admin, This setting is displayed on the
+    | login page.
+    |
+    */
+    'name' => 'EzCarpool Driver Dashboard',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Laravel-admin logo
+    |--------------------------------------------------------------------------
+    |
+    | The logo of all admin pages. You can also set it as an image by using a
+    | `img` tag, eg '<img src="http://logo-url" alt="Admin logo">'.
+    |
+    */
+    'logo' => '<b>EzCarpool</b> Driver',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Laravel-admin mini logo
+    |--------------------------------------------------------------------------
+    |
+    | The logo of all admin pages when the sidebar menu is collapsed. You can
+    | also set it as an image by using a `img` tag, eg
+    | '<img src="http://logo-url" alt="Admin logo">'.
+    |
+    */
+    'logo-mini' => '<b>Ez</b>',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Login page background image
+    |--------------------------------------------------------------------------
+    |
+    | This value is used to set the background image of login page.
+    |
+    */
+    'login_background_image' => '',
+
+];
