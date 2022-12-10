4bbf9716a5a06a6156b5288897a2b448a2b75e1b
ETHAN23\pangy <Ethant Pang>
2022-12-11 00:47:45 +0800

    feat : update profile information

--- resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.vue
+++ resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.vue
@@ -1,10 +1,10 @@
 <script setup>
-import InputError from '@/Components/InputError.vue';
-import InputLabel from '@/Components/InputLabel.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
-import TextInput from '@/Components/TextInput.vue';
-import { Link, useForm, usePage } from '@inertiajs/inertia-vue3';
-import { ref } from 'vue';
+import InputError from "@/Components/InputError.vue";
+import InputLabel from "@/Components/InputLabel.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import TextInput from "@/Components/TextInput.vue";
+import { Link, useForm, usePage } from "@inertiajs/inertia-vue3";
+import { ref } from "vue";
 
 const props = defineProps({
     mustVerifyEmail: Boolean,
@@ -15,21 +15,33 @@ const user = usePage().props.value.auth.user;
 
 const form = useForm({
     name: user.name,
-    email: user.email,
+    birthday: user.birthday,
 });
 </script>
 
 <template>
     <section>
         <header>
-            <h2 class="text-lg font-medium text-gray-900">Profile Information</h2>
+            <h2 class="text-lg font-medium text-gray-900">
+                Profile Information
+            </h2>
 
             <p class="mt-1 text-sm text-gray-600">
                 Update your account's profile information and email address.
             </p>
         </header>
 
-        <form @submit.prevent="form.patch(route('profile.update'))" class="mt-6 space-y-6">
+        <form
+            @submit.prevent="form.patch(route('profile.update'))"
+            class="mt-6 space-y-6"
+        >
+            <div>
+                <InputLabel for="username" value="Name" />
+
+                <p class="mt-1 block w-full font-semibold">
+                    {{ user.username }}
+                </p>
+            </div>
             <div>
                 <InputLabel for="name" value="Name" />
 
@@ -45,23 +57,32 @@ const form = useForm({
 
                 <InputError class="mt-2" :message="form.errors.name" />
             </div>
-
             <div>
                 <InputLabel for="email" value="Email" />
 
-                <TextInput
-                    id="email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    v-model="form.email"
-                    required
-                    autocomplete="email"
-                />
-
+                <p class="mt-1 block w-full font-semibold">
+                    {{ user.email }}
+                </p>
                 <InputError class="mt-2" :message="form.errors.email" />
             </div>
+            <div>
+                <InputLabel for="birthday" value="Birthday" />
 
-            <div v-if="props.mustVerifyEmail && user.email_verified_at === null">
+                <TextInput
+                    id="birthday"
+                    type="date"
+                    class="mt-1 block w-full"
+                    v-model="form.birthday"
+                    required
+                    autofocus
+                    autocomplete="birthday"
+                />
+
+                <InputError class="mt-2" :message="form.errors.birthday" />
+            </div>
+            <div
+                v-if="props.mustVerifyEmail && user.email_verified_at === null"
+            >
                 <p class="text-sm mt-2 text-gray-800">
                     Your email address is unverified.
                     <Link
@@ -85,8 +106,17 @@ const form = useForm({
             <div class="flex items-center gap-4">
                 <PrimaryButton :disabled="form.processing">Save</PrimaryButton>
 
-                <Transition enter-from-class="opacity-0" leave-to-class="opacity-0" class="transition ease-in-out">
-                    <p v-if="form.recentlySuccessful" class="text-sm text-gray-600">Saved.</p>
+                <Transition
+                    enter-from-class="opacity-0"
+                    leave-to-class="opacity-0"
+                    class="transition ease-in-out"
+                >
+                    <p
+                        v-if="form.recentlySuccessful"
+                        class="text-sm text-gray-600"
+                    >
+                        Saved.
+                    </p>
                 </Transition>
             </div>
         </form>
